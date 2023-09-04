b4dff88ba054bd1348aa225ff4b3507b63c84f69
Ldw_Tester <746131829@qq.com>
2023-09-04 15:57:29 +0800

    整理git

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/evn python
 # -*- encoding: utf-8 -*-
-import unittest
-from LDW_Automate_POM.TestCase.test_Aloginin_func import Test_KX_jishou
-if __name__=="__main__":
-    testui=unittest.TestSuite()
-    testui.main(Test_KX_jishou('KX_js'))
+# import unittest
+# from LDW_Automate_POM.TestCase.test_Aloginin_func import Test_KX_jishou
+# if __name__=="__main__":
+#     testui=unittest.TestSuite()
+#     testui.main(Test_KX_jishou('KX_js'))
 
 
 
